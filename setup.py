0c24d8ad4afd066b8cbc4c3f21be921491eb9106
soshi-mizutani <soshi.mizutani@oist.jp>
2021-09-25 12:52:37 +0900

    included python3.9

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = ['Click', ]
 
 test_requirements = ['pytest>=3', ]
 
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+	'Programming Language :: Python :: 3.9',
     ],
     description="learn python OPP by implementing objects in One Piece ",
     entry_points={
